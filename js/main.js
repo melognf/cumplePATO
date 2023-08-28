7cde2312ea6226b8a1722bc0d870164a9fae9cae
melognf <melognf@yahoo.com.ar>
2023-08-28 17:31:33 -0300

    horario

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
         year: 2023, // required
         month: 9, // required
         day: 11, // required
-        hours: 0, // Default is 0 [0-23] integer
+        hours: 16, // Default is 0 [0-23] integer
         minutes: 0, // Default is 0 [0-59] integer
         seconds: 0, // Default is 0 [0-59] integer
         words: { //words displayed into the countdown
